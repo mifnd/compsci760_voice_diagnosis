f5a0db831d384fa0dc6b9f9b924b198e9bb8abd4
mifnd <180616816+mifnd@users.noreply.github.com>
2025-05-11 21:12:52 +1200

    add mfcc of pitch up

---
+++ code/feature_extraction/fe_aug_pitch_up_10mfcc_mean.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+from fe_mfcc_mean import create_csv_with_mfcc_mean
+
+# Compute mean of the first 10 mfcc vectors for the dataset "aug_pitch_up",
+# where the pitch has been shifted up.
+root = Path("../..").resolve()
+recordings_path = root / "data/interim/aug_pitch_up"
+csv_output_path = root / "data/processed/aug_pitch_up_10mfcc_mean.csv"
+
+create_csv_with_mfcc_mean(recordings_path=recordings_path,
+                          csv_output_path=csv_output_path,
+                          n_mfcc=10)
