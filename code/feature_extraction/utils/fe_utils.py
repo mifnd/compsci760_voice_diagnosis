b318d7dbee0cc77206340f56394ac2ee409e3223
mifnd <180616816+mifnd@users.noreply.github.com>
2025-05-13 11:48:26 +1200

    add compute_median_mfcc util

--- code/feature_extraction/utils/fe_utils.py
+++ code/feature_extraction/utils/fe_utils.py
@@ -9,3 +9,16 @@ def compute_mean_mfcc(file_path: Path, n_mfcc: int):
     y, sr = librosa.load(file_path)
     mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=n_mfcc)
     return np.mean(mfcc, axis=1)
+
+
+def compute_median_mfcc(file_path: Path, n_mfcc: int):
+    y, sr = librosa.load(file_path)
+    mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=n_mfcc)
+    return np.median(mfcc, axis=1)
+
+
+def compute_mfcc(file_path: Path, n_mfcc: int, avg_type: str):
+    if avg_type == "median":
+        compute_median_mfcc(file_path=file_path, n_mfcc=n_mfcc)
+    else:
+        compute_mean_mfcc(file_path=file_path, n_mfcc=n_mfcc)
