b24b36390c7faa901871e7d60eef71e8f5c4c93c
Neko-box <3282819732@qq.com>
2025-05-26 23:07:54 +1200

    improved model for mfcc40 and changed structure for splited files

--- code/modeling/xgb_10mfcc_median.py
+++ code/modeling/xgb_10mfcc_median.py
@@ -1,9 +1,8 @@
 from xgb_runner import run_xgb_pipeline
 
 if __name__ == "__main__":
-    # real data paths
-    real_paths = ["data/processed/10mfcc_median.csv"]
-    # augmented data paths
-    aug_paths = [
-        ]
-    run_xgb_pipeline(real_paths, aug_paths)
+    main_csv = "data/processed/10mfcc_mean.csv"
+    train_txt = "data/train1.txt"
+    test_txt = "data/test1.txt"
+    aug_csv_list = []
+    run_xgb_pipeline(main_csv, train_txt, test_txt, aug_csv_list)
